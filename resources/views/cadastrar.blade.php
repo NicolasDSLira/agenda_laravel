e03ce58edc35d5a0ff370b4a0850060a373ce9a7
NicolasDSLira <80727008+NicolasDSLira@users.noreply.github.com>
2021-06-28 14:31:47 -0300

    Update cadastrar.blade.php

--- resources/views/cadastrar.blade.php
+++ resources/views/cadastrar.blade.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+        <title>Cadastrar contato</title>
         <!-- Styles -->
         <link rel="stylesheet" type="text/css" href="{{url('assets/css/style.cadastro.css')}}">
         <!-- Mascara telefone -->
